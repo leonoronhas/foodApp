cf410a0f724a043739ffe2c082dadce73654ad8a
leonoronsantos <leosud099@gmail.com>
2020-04-07 12:13:39 -0600

    Implemented and fixed meals details screen

--- my-food-app/screens/MealDetailScreen.js
+++ my-food-app/screens/MealDetailScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Button, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { MEALS } from '../data/dummy-data';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import HeaderButton from '../components/HeaderButton';
 
+
+const ListItem = props => {
+   return (
+      <View style={styles.listItem}>
+         <Text style={{ fontFamily: 'roboto' }}>{props.children}</Text>
+      </View>
+   );
+};
 /*===============================================
 =
 =
@@ -15,12 +24,41 @@ const MealDetailScreen = props => {
    const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
    return (
-      <View style={styles.screen}>
-         <Text>The Meal Detail Screen!</Text>
-         <Button title="Go Home" onPress={() => {
-            props.navigation.popToTop();
-         }} />
-      </View>
+      <ScrollView>
+         <View style={styles.imageContainer}>
+            <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+         </View>
+         <View style={styles.details}>
+            <Text style={{ fontFamily: 'roboto-bold' }}>{selectedMeal.duration} m</Text>
+            <Text style={{ fontFamily: 'roboto-bold' }}>{selectedMeal.complexity.toUpperCase()}</Text>
+            <Text style={{ fontFamily: 'roboto-bold' }}>{selectedMeal.affordability.toUpperCase()}</Text>
+         </View>
+         <View style={styles.icons}>
+            <Text style={styles.title}>Ingredients </Text>
+            <MaterialCommunityIcons
+               title="IngredientsIcon"
+               name='food-variant'
+               size={19}
+            />
+         </View>
+
+         {selectedMeal.ingredients.map(ingredient => (  // Display content in the array
+            <ListItem key={ingredient}>{ingredient}</ListItem>
+         ))}
+
+         <View style={styles.icons}>
+            <Text style={styles.title}>Steps</Text>
+            <MaterialCommunityIcons
+               title="StepsIcon"
+               name='walk'
+               size={19}
+            />
+         </View>
+
+         {selectedMeal.steps.map(step => (  // Display content in the array
+            <ListItem key={step}>{step}</ListItem>
+         ))}
+      </ScrollView>
    );
 };
 
@@ -34,7 +72,7 @@ MealDetailScreen.navigationOptions = (navigationData) => {
             <Item
                title='Favorite'
                iconName='ios-star'
-               onPress={() => {}}
+               onPress={() => { }}
             />
          </HeaderButtons>
       )
@@ -42,10 +80,39 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-   screen: {
-      flex: 1,
+   imageContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: 10,
+
+   },
+   image: {
+      width: '95%',
+      height: 200,
+      borderRadius: 10
+   },
+   details: {
+      flexDirection: 'row',
+      padding: 15,
+      justifyContent: 'space-around'
+   },
+   title: {
+      fontFamily: 'roboto-bold',
+      fontSize: 20,
+      textAlign: 'center'
+   },
+   icons: {
+      flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center'
+   },
+   listItem: {
+      marginVertical: 10,
+      marginHorizontal: 20,
+      borderColor: '#ccc',
+      borderWidth: 1,
+      padding: 10,
+      borderRadius: 10
    }
 });
 
